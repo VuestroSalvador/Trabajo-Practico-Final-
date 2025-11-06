695722ac43018d800e648c3e28f64cbc97e2432d
VuestroSalvador <salvadordelfabro@gmail.com>
2025-11-06 13:03:15 -0300

    modificaciones

--- TrabajoFinalGarciaDelfabro/obj/Debug/net8.0-windows/TrabajoFinalGarciaDelfabro.AssemblyInfo.vb
+++ TrabajoFinalGarciaDelfabro/obj/Debug/net8.0-windows/TrabajoFinalGarciaDelfabro.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("TrabajoFinalGarciaDelfabro"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1801da1a6d9befa6838932eddf0e658c7119bf7e"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c41ee3103835c1c158eb5814c671e89491d8ccaa"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("TrabajoFinalGarciaDelfabro"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("TrabajoFinalGarciaDelfabro"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
